e13190b9954b09ae338ebc28ed471c4084dd4016
Dana <dsniezko@gmail.com>
2014-02-08 14:56:10 -0800

    cleaner

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 /* 
-* Learning node.js via common work tasks. 
-* This script should take a CSV with a list of identifiers and address, append congressional
-* district and bioguide (unique id) for members of Congress
-* 
-* See test.csv for format example
-*/
-
-
-
+ * Learning node.js via common work tasks.
+ * This script should take a CSV with a list of identifiers and address, append congressional
+ * district and bioguide (unique id) for members of Congress
+ *
+ * See test.csv for format example
+ */
 // set geocoding provider, using google for example, DSTK on local network for real geocoding
 var geocoderProvider = 'google';
 var httpAdapter = 'http';
@@ -26,32 +23,35 @@ var api = require("sunlight-congress-api");
 api.init("xxx");
 
 // grab district # and bioguide_id of rep, senators
-function findCongress (api,lat,lon,callback){
-	api.legislatorsLocate().filter("latitude",lat ).filter("longitude",lon).call(function(data){ 
-						var mocList=Array();
-						mocList.push(data.results[0].district);
-						for (var i=0; i < data.results.length; i++) {  
-							mocList.push(data.results[i].bioguide_id);  					  
-	    				}
-	    				callback(mocList);  					  
-	});
+function findCongress(api, lat, lon, callback) {
+    api.legislatorsLocate().filter("latitude", lat).filter("longitude", lon).call(function (data) {
+        var mocList = Array();
+        mocList.push(data.results[0].district);
+        for (var i = 0; i < data.results.length; i++) {
+            mocList.push(data.results[i].bioguide_id);
+        }
+        callback(mocList);
+    });
 }
 
-
-
-csv(inStr, {headers : true,ignoreEmpty: true})
- .on("data",function(data){
-     var toLoc = data.address+" "+data.city+","+data.state+" "+data.postal;
-     var geoInfo=new Array();
-     geocoder.geocode(toLoc,function(err,res) {
-     	if (err) { console.log(err.toString()+":"+toLoc); outErr.write(data.ident.toString()+","+toLoc+"\n") }
-     	else {
-     		geoInfo.push(res[0].latitude,res[0].longitude);
-	     	findCongress(api,res[0].latitude,res[0].longitude,function(d){
-	     		// prep a line and write out
-	     		var tmp = data.ident.toString()+","+data.state.toString()+","+geoInfo.concat(d).join()+"\n";
-	     		outStr.write(tmp);
-	     	});
-     	 }
-     });
- }).parse();
+csv(inStr, {
+    headers: true,
+    ignoreEmpty: true
+})
+    .on("data", function (data) {
+        var toLoc = data.address + " " + data.city + "," + data.state + " " + data.postal;
+        var geoInfo = new Array();
+        geocoder.geocode(toLoc, function (err, res) {
+            if (err) {
+                console.log(err.toString() + ":" + toLoc);
+                outErr.write(data.ident.toString() + "," + toLoc + "\n")
+            } else {
+                geoInfo.push(res[0].latitude, res[0].longitude);
+                findCongress(api, res[0].latitude, res[0].longitude, function (d) {
+                    // prep a line and write out
+                    var tmp = data.ident.toString() + "," + data.state.toString() + "," + geoInfo.concat(d).join() + "\n";
+                    outStr.write(tmp);
+                });
+            }
+        });
+    }).parse();
